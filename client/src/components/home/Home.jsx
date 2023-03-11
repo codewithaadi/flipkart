db1157b002f4dbe9e10b7e240232859c6cbd56ad
Aaditya Mishra <codewithaadi31@gmail.com>
2023-03-11 11:30:35 +0530

    Redux Implementation.

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 //components
 import Navbar from './Navbar';
 import Banner from './Banner';
+import { getProducts } from '../../redux/actions/productActions';
 
 //MUI
-import { Box,styled } from '@mui/material';
+import { Box, styled } from '@mui/material';
 
 const Component = styled(Box)`
     padding: 18px 0 10px 0;
@@ -13,12 +16,20 @@ const Component = styled(Box)`
 `
 
 export default function Home() {
+
+    const { products } = useSelector(state => state.getProducts)
+    //Custom Hook
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getProducts());
+    }, [dispatch])
+
     return (
         <>
             <Navbar />
             <Component>
                 <Banner />
-
             </Component>
         </>
     )
---
+++ client/src/redux/store.js
@@ -0,0 +1,19 @@
+import {createStore,combineReducers, applyMiddleware} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+
+//Components
+import { getProductsReducer } from './reducers/productReducer';
+
+const reducer = combineReducers({
+    getProducts : getProductsReducer
+})
+
+const middleware = [thunk];
+
+const store = createStore(
+    reducer,
+    composeWithDevTools(applyMiddleware(...middleware))
+)
+
+export default store;
